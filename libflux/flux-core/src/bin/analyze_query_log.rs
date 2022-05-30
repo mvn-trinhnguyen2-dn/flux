8c6e6eb30ee562fca30d8a849b6373b6e2e80301
Markus Westerlind <mwesterlind@influxdata.com>
2022-05-30 15:03:00 +0200

    feat: Parallelize the query log checker

--- libflux/flux-core/src/bin/analyze_query_log.rs
+++ libflux/flux-core/src/bin/analyze_query_log.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{Error, Result};
+use rayon::prelude::*;
 use structopt::StructOpt;
 
 use fluxcore::semantic::{self, Analyzer};
@@ -27,24 +28,28 @@ fn main() -> Result<()> {
     let (prelude, imports, _sem_pkgs) =
         semantic::bootstrap::infer_stdlib_dir(&stdlib_path, semantic::AnalyzerConfig::default())?;
 
-    let mut analyzer = Analyzer::new(
-        (&prelude).into(),
-        imports,
-        semantic::AnalyzerConfig::default(),
-    );
+    let analyzer = || {
+        Analyzer::new(
+            (&prelude).into(),
+            &imports,
+            semantic::AnalyzerConfig::default(),
+        )
+    };
 
     let (prelude, imports, _sem_pkgs) =
         semantic::bootstrap::infer_stdlib_dir(&stdlib_path, label_polymorphism_config.clone())?;
 
-    let mut label_polymorphism_analyzer = Analyzer::new(
-        (&prelude).into(),
-        imports,
-        label_polymorphism_config.clone(),
-    );
+    let label_polymorphism_analyzer = || {
+        Analyzer::new(
+            (&prelude).into(),
+            &imports,
+            label_polymorphism_config.clone(),
+        )
+    };
 
     if app.database.extension() == Some(std::ffi::OsStr::new("flux")) {
         let source = std::fs::read_to_string(&app.database)?;
-        label_polymorphism_analyzer
+        label_polymorphism_analyzer()
             .analyze_source("".into(), "".into(), &source)
             .map_err(|err| err.error.pretty_error())?;
         return Ok(());
@@ -52,90 +57,128 @@ fn main() -> Result<()> {
 
     let connection = rusqlite::Connection::open(&app.database)?;
 
+    let (tx, rx) = crossbeam_channel::bounded(128);
+
+    let (final_tx, final_rx) = crossbeam_channel::bounded(128);
+
     let mut count = 0;
-    for (i, source) in connection
-        .prepare("SELECT source FROM query limit 100000")?
-        .query_map([], |row| row.get(0))?
-        .enumerate()
-    {
-        if let Some(skip) = app.skip {
-            if i < skip {
-                continue;
-            }
-        }
 
-        let source: String = source?;
-
-        // eprintln!("{}", source);
-
-        let analyzer = std::panic::AssertUnwindSafe(&mut analyzer);
-        let current_result = match std::panic::catch_unwind(|| {
-            let analyzer = analyzer;
-            analyzer.0.analyze_source("".into(), "".into(), &source)
-        }) {
-            Ok(x) => x,
-            Err(_) => panic!("Panic at source {}: {}", i, source),
-        };
-
-        let label_polymorphism_analyzer =
-            std::panic::AssertUnwindSafe(&mut label_polymorphism_analyzer);
-        let label_polymorphism_result = match std::panic::catch_unwind(|| {
-            let label_polymorphism_analyzer = label_polymorphism_analyzer;
-            label_polymorphism_analyzer
-                .0
-                .analyze_source("".into(), "".into(), &source)
-        }) {
-            Ok(x) => x,
-            Err(_) => panic!("Panic at source {}: {}", i, source),
-        };
-
-        match (current_result, label_polymorphism_result) {
-            (Ok(_), Ok(_)) => (),
-            (Err(err), Ok(_)) => {
-                eprintln!("{}", source);
-
-                eprintln!(
-                    "Missing errors when label polymorphism is enabled: {}",
-                    err.error.pretty(&source)
-                );
-                eprintln!("-------------------------------");
-            }
-            (Ok(_), Err(err)) => {
-                eprintln!("{}", source);
-
-                eprintln!(
-                    "New errors when label polymorphism is enabled: {}",
-                    err.error.pretty(&source)
-                );
-                eprintln!("-------------------------------");
-            }
-            (Err(current_err), Err(label_polymorphism_err)) => {
-                if false {
-                    let current_err = current_err.error.pretty(&source);
-                    let label_polymorphism_err = label_polymorphism_err.error.pretty(&source);
-                    if current_err != label_polymorphism_err {
-                        eprintln!("{}", source);
-
-                        eprintln!(
-                            "Different when label polymorphism is enabled:\n{}",
-                            pretty_assertions::StrComparison::new(
-                                &current_err,
-                                &label_polymorphism_err,
-                            )
-                        );
-                        eprintln!("-------------------------------");
+    let (r, r2, _) = join3(
+        move || {
+            for (i, result) in connection
+                .prepare("SELECT source FROM query limit 100000")?
+                .query_map([], |row| row.get(0))?
+                .enumerate()
+            {
+                if let Some(skip) = app.skip {
+                    if i < skip {
+                        continue;
                     }
                 }
-            }
-        }
-        count += 1;
 
-        if count % 100 == 0 {
-            eprintln!("Checked {} queries", count);
-        }
-    }
+                let source: String = result?;
+                tx.send((i, source))?;
+            }
+
+            Ok::<_, Error>(())
+        },
+        move || {
+            rx.into_iter()
+                .par_bridge()
+                .try_for_each(|(i, source): (usize, String)| {
+                    // eprintln!("{}", source);
+
+                    let current_result = match std::panic::catch_unwind(|| {
+                        analyzer().analyze_source("".into(), "".into(), &source)
+                    }) {
+                        Ok(x) => x,
+                        Err(_) => panic!("Panic at source {}: {}", i, source),
+                    };
+
+                    let label_polymorphism_result = match std::panic::catch_unwind(|| {
+                        label_polymorphism_analyzer().analyze_source("".into(), "".into(), &source)
+                    }) {
+                        Ok(x) => x,
+                        Err(_) => panic!("Panic at source {}: {}", i, source),
+                    };
+
+                    match (current_result, label_polymorphism_result) {
+                        (Ok(_), Ok(_)) => (),
+                        (Err(err), Ok(_)) => {
+                            eprintln!("### {}", i);
+                            eprintln!("{}", source);
+
+                            eprintln!(
+                                "Missing errors when label polymorphism is enabled: {}",
+                                err.error.pretty(&source)
+                            );
+                            eprintln!("-------------------------------");
+                        }
+                        (Ok(_), Err(err)) => {
+                            eprintln!("### {}", i);
+                            eprintln!("{}", source);
+
+                            eprintln!(
+                                "New errors when label polymorphism is enabled: {}",
+                                err.error.pretty(&source)
+                            );
+                            eprintln!("-------------------------------");
+                        }
+                        (Err(current_err), Err(label_polymorphism_err)) => {
+                            if false {
+                                let current_err = current_err.error.pretty(&source);
+                                let label_polymorphism_err =
+                                    label_polymorphism_err.error.pretty(&source);
+                                if current_err != label_polymorphism_err {
+                                    eprintln!("{}", source);
+
+                                    eprintln!(
+                                        "Different when label polymorphism is enabled:\n{}",
+                                        pretty_assertions::StrComparison::new(
+                                            &current_err,
+                                            &label_polymorphism_err,
+                                        )
+                                    );
+                                    eprintln!("-------------------------------");
+                                }
+                            }
+                        }
+                    }
+
+                    final_tx.send(())?;
+
+                    Ok::<_, Error>(())
+                })
+        },
+        || {
+            for _ in final_rx {
+                count += 1;
+
+                if count % 100 == 0 {
+                    eprintln!("Checked {} queries", count);
+                }
+            }
+        },
+    );
+
+    r?;
+    r2?;
 
     eprintln!("Done! Checked {} queries", count);
 
     Ok(())
+}
+
+fn join3<A, B, C>(
+    a: impl FnOnce() -> A + Send,
+    b: impl FnOnce() -> B + Send,
+    c: impl FnOnce() -> C + Send,
+) -> (A, B, C)
+where
+    A: Send,
+    B: Send,
+    C: Send,
+{
+    let (a, (b, c)) = rayon::join(a, || rayon::join(b, c));
+    (a, b, c)
 }
